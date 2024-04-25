df0c6dca992f74fec1d465f63bb766eea021ae72
claraatdtu <s232764@dtu.dk>
2024-04-25 09:51:44 +0200

    Add Arduino code

---
+++ smartwateringsystem/thingspeak/thingspeak.ino
@@ -0,0 +1,135 @@
+// Including the ESP8266 WiFi library
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <ThingSpeak.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+
+// DHT sensor 
+#define DHTTYPE DHT11   // DHT 11
+
+
+// network details
+//const char* ssid = "Nishtman";
+//const char* password = "Nishtman20";
+//const char* ssid = "SRK";
+//const char* password = "12345678";
+const char* ssid = "recherche...";
+const char* password = "mpsi2areuhere";
+
+WiFiClient client;
+
+const int DHTPin = D3;
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+
+unsigned long channelID = 2519764; // channel
+const char * myWriteAPIKey = "LLIBJC6Q2HMOE826"; // WRITE API key
+const char* server = "api.thingspeak.com";
+
+const int postingInterval = 20 * 1000; // post data every 20 seconds
+
+// only runs once on boot
+void setup() {
+  // Initializing serial port for debugging purposes
+  Serial.begin(115200);
+  delay(10);
+
+  dht.begin();
+  
+  // Connecting to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+}
+
+// runs over and over again
+void loop() {
+  ThingSpeak.begin(client);
+  if (client.connect(server, 80)) {
+    
+    // Measure Signal Strength (RSSI) of Wi-Fi connection
+    long rssi = WiFi.RSSI();
+
+    Serial.print("RSSI: ");
+    Serial.println(rssi); 
+
+
+    ThingSpeak.setField(4,rssi);
+
+// **** This part reads  sensors and calculates
+    float h = dht.readHumidity();
+            // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+            // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+            // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      strcpy(celsiusTemp,"Failed");
+      strcpy(fahrenheitTemp, "Failed");
+      strcpy(humidityTemp, "Failed");         
+      }
+    else{
+              // Computes temperature values in Celsius + Fahrenheit and Humidity
+      float hic = dht.computeHeatIndex(t, h, false);       
+      dtostrf(hic, 6, 2, celsiusTemp);             
+      float hif = dht.computeHeatIndex(f, h);
+      dtostrf(hif, 6, 2, fahrenheitTemp);         
+      dtostrf(h, 6, 2, humidityTemp);
+
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.print(" *F");
+              Serial.print("Humidity: ");
+              Serial.print(h);
+              Serial.print(" %\t Temperature: ");
+              Serial.print(t);
+              Serial.print(" *C ");
+              Serial.print(f);
+              Serial.print(" *F\t Heat index: ");
+              Serial.print(hic);
+              Serial.print(" *C ");
+              Serial.print(hif);
+              Serial.println(" *F");
+        //end of sensor readings
+      ThingSpeak.setField(1,t);
+      ThingSpeak.setField(2,f);
+      ThingSpeak.setField(3,h);
+      }
+            
+      ThingSpeak.writeFields(channelID, myWriteAPIKey);
+  }
+    client.stop();
+
+  // wait and then post again
+  delay(postingInterval);
+}
---
+++ smartwateringsystem/webserver_34322/webserver_34322.ino
@@ -0,0 +1,95 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+
+ESP8266WiFiMulti wifiMulti;
+ESP8266WebServer server(80);
+
+void handleRoot();
+void handleNotFound();
+void openweather();
+
+void setup() {
+    Serial.begin(9600);
+    delay(10);
+
+    // Connect to WiFi network
+    //wifiMulti.addAP("Nishtman", "Nishtman20");
+    //wifiMulti.addAP("Jakobs_iphone", "LarsDrip");
+    //wifiMulti.addAP("SRK", "12345678");
+    wifiMulti.addAP("recherche...", "mpsi2areuhere");
+
+    Serial.println();
+    Serial.print("Connecting ...");
+
+    while (wifiMulti.run() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected to ");
+    Serial.println(WiFi.SSID());
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    if (MDNS.begin("iot")) {
+        Serial.println("mDNS responder started");
+    } else {
+        Serial.println("Error setting up MDNS responder!");
+    }
+
+    server.on("/", HTTP_GET, handleRoot);
+    server.on("/weather", HTTP_GET, openweather);
+    server.onNotFound(handleNotFound);
+
+    server.begin();
+    Serial.println("Server started");
+}
+
+void loop() {
+    server.handleClient();
+}
+
+void handleRoot() {
+    String page = "<html><head><title>Internet of Things - Demonstration</title><meta charset=\"utf-8\"></head><body>";
+    page += "<h1>Welcome!</h1>";
+    page += "<p>Click the following button to get current weather information:</p>";
+    page += "<form action=\"/weather\" method=\"GET\"><input type=\"submit\" value=\"Get Weather\"></form>";
+    page += "</body></html>";
+    server.send(200, "text/html", page);
+}
+
+void openweather() {
+    const char* endpoint = "http://api.openweathermap.org/data/2.5/weather?q=Kongens%20Lyngby,dk&APPID=2bd047f4071440aff1b938b1fb2942e4&units=metric";
+    HTTPClient http;
+    WiFiClient client;
+
+    http.begin(client, endpoint);
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        DynamicJsonDocument doc(2000);
+        deserializeJson(doc, payload);
+
+        JsonObject main = doc["main"];
+        float main_temp = main["temp"];
+        float main_hum = main["humidity"];
+
+        char html[500];
+        sprintf(html, "The temperature is %.1f Celsius degrees and the humidity is %.0f .", main_temp, main_hum);
+        server.send(200, "text/html", html);
+    } else {
+        server.send(500, "text/plain", "Failed to retrieve weather data");
+    }
+
+    http.end();
+}
+
+void handleNotFound() {
+    server.send(404, "text/plain", "404 Not Found");
+}
