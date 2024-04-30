3816af138ac086f6d4fffe5b722380ee208ca119
MarkcMalloy <markcmalloy96@gmail.com>
2024-04-30 14:00:57 +0200

    Added some test code to control the pump 1

--- smartwateringsystem/mkr_wifi_iot/sketch_apr27a/sketch_apr27a.ino
+++ smartwateringsystem/mkr_wifi_iot/sketch_apr27a/sketch_apr27a.ino
@@ -1,4 +1,3 @@
-
 #include <Arduino_MKRIoTCarrier.h>
 #include <WiFiNINA.h>
 MKRIoTCarrier carrier;
@@ -19,9 +18,9 @@ int status = WL_IDLE_STATUS;       // the Wifi radio's status
 WiFiServer server(23); // TCP server on port 23 (Typically used for Telnet, change as needed)
 
 void setup() {
-    Serial.begin(9600);
     carrier.noCase();
     carrier.begin();
+    Serial.begin(9600);
     while (!Serial);
 
     // Attempt to connect to Wifi network:
@@ -150,6 +149,8 @@ void handlePostRequest(WiFiClient &client, const String &url) {
     // Handle POST requests here
     if (url.startsWith("/pump1")) {
         pump1ON();
+        delay(2000)
+        pump1Off();
         sendHttpResponse(client, 200, "{}");
     }
     if (url.startsWith("/pump2")) {
